0c660517073bd63165ad4ead741fa3ad736219f5
kaoudovic <kaoudovic@gmail.com>
2020-05-16 18:08:57 +0200

    reade more and create post blog

--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView,EntryView,CreateEntryview
 
 urlpatterns = [
     path('',HomeView.as_view(),name='blog-home'),
-
+    path('entry/<int:pk>/',EntryView.as_view(),name='entry-detail'),
+    path('create_entry/',CreateEntryview.as_view(success_url='/'),name='create_entry'),
 ]
--- blog/entries/views.py
+++ blog/entries/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render,HttpResponse,redirect
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Entry
 
+
 class HomeView(ListView):
-    model =Entry
+    model=Entry
     template_name = 'entries/index.html'
+    context_object_name = "blog_entries"
+
+
+class EntryView(DetailView):
+    model = Entry
+    template_name = 'entries/entry_detail.html'
+
+
+class CreateEntryview(CreateView):
+    model = Entry
+    template_name = 'entries/create_entry.html'
+    fields = ['entry_title','entry_text']
+
+    def form_valid(self, form):
+        form.instance.entry_auther=self.request.user
+        return super().form_valid(form)
 
 
