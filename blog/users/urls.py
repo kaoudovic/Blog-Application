e4eeaa2f1fa4dbf5c5c45e90b8a69bb787d3e5dc
kaoudovic <kaoudovic@gmail.com>
2020-05-20 08:34:05 +0200

    finsh  edit project

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import register
 urlpatterns = [
     path('register/',register,name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name="users/templates/login.html"),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name="users/templates/logout.html"), name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name="login.html"),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
 
 ]
